a717a8ad19b1f41ba4f7befe72e4562ffd2fd3a6
Ben <benjhardie@icloud.com>
2024-01-18 11:55:49 +0000

    feat: add getPlayerChoice function

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const OPTIONS = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   return OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
 }
+
+function getPlayerChoice() {
+  let playerSelection;
+
+  while (true) {
+    playerSelection = prompt("Make your choice:", "").toLowerCase();
+
+    if (!OPTIONS.includes(playerSelection)) {
+      alert("Please choose a valid option (rock, paper or scissors)");
+      continue;
+    }
+    break;
+  }
+
+  return playerSelection;
+}
+
+computerSelection = getComputerChoice();
+playerSelection = getPlayerChoice();
